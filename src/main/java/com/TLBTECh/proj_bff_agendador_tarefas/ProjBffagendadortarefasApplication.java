fd559089899c505946fcc303a1a1d2259a2ab808
Taciano <taciolb@gmail.com>
2025-11-06 20:20:25 -0300

    adicionado esqueleto do projeto

---
+++ src/main/java/com/TLBTECh/proj_bff_agendador_tarefas/ProjBffagendadortarefasApplication.java
@@ -0,0 +1,13 @@
+package com.TLBTECh.proj_bff_agendador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjBffagendadortarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjBffagendadortarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TLBTECh/proj_bff_agendador_tarefas/ProjBffagendadortarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TLBTECh.proj_bff_agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjBffagendadortarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
