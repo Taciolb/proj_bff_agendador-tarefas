8f01ecf97bcc0cefd6086a86dbe60a45d0c2138e
Taciano <taciolb@gmail.com>
2025-11-13 21:03:14 -0300

    adicionado Cron

--- src/main/java/com/TLBTECh/proj_bff_agendador_tarefas/ProjBffagendadortarefasApplication.java
+++ src/main/java/com/TLBTECh/proj_bff_agendador_tarefas/ProjBffagendadortarefasApplication.java
@@ -3,9 +3,11 @@ package com.TLBTECh.proj_bff_agendador_tarefas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class ProjBffagendadortarefasApplication {
 
 	public static void main(String[] args) {
